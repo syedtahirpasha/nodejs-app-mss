1bcaf892b3289991ca826fbcc80f5fc1c1e0c911
syedtahirpasha <139238732+syedtahirpasha@users.noreply.github.com>
2025-08-02 00:12:09 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -2,16 +2,16 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
   serverUrl: 'http://43.205.228.64:9980/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'sqa_b130d7147f33bd68f581f8d042fe5f8aa1e724d3',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
+  options: {
+    'sonar.projectDescription': 'This is a Node JS application',
+    'sonar.projectName': 'Node JS Application - Sample',
+    'sonar.projectKey': 'NodeJsMSS',
+    'sonar.login': 'sqa_b130d7147f33bd68f581f8d042fe5f8aa1e724d3',
+    'sonar.projectVersion': '1.0',
+    'sonar.language': 'js',
+    'sonar.sourceEncoding': 'UTF-8',
+    'sonar.sources': 'src',  // Changed from '.' to 'src'
+    // 'sonar.tests': 'specs',
+    // 'sonar.inclusions': 'src/**'  // More precise inclusion pattern
+  }
 }, () => {});
