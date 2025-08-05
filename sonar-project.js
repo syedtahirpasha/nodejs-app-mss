46adca43d8afb052ba125fc62e6294a46d18a2dd
syedtahirpasha <139238732+syedtahirpasha@users.noreply.github.com>
2025-08-05 20:18:56 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,17 @@
 const sonarqubeScanner = require('sonarqube-scanner');
-const serverUrl = process.env.SONAR_HOST_URL || 'http://13.233.231.77:9000/';
-const token = process.env.SONAR_TOKEN || 'sqa_5cbaeef3c359febf23be336c49424fffce37d09f';
 
 sonarqubeScanner({
-  serverUrl: serverUrl,
-  options: {
-    'sonar.projectDescription': 'This is a Node JS application',
-    'sonar.projectName': 'Node JS Application - Sample',
-    'sonar.projectKey': 'NodeJsMSS',
-    'sonar.login': token,
-    'sonar.projectVersion': '1.0',
-    'sonar.language': 'js',
-    'sonar.sourceEncoding': 'UTF-8',
-    'sonar.sources': '.',  // Corrected to root directory
-    'sonar.exclusions': 'node_modules/**'
-  }
+  serverUrl: 'http://13.233.231.77:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMSS',
+	    'sonar.login': 'sqa_1e048b794e5b6dc896e7e9c3c7585517b202b51d',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
 }, () => {});
