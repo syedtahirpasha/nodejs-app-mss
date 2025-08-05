b98e6446c45215de43896802bdc0f196a0db933d
syedtahirpasha <139238732+syedtahirpasha@users.noreply.github.com>
2025-08-05 17:44:19 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,18 @@
 const sonarqubeScanner = require('sonarqube-scanner');
+const serverUrl = process.env.SONAR_HOST_URL || 'http://13.233.231.77:9000/';
+const token = process.env.SONAR_TOKEN || '';
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.231.77:9000/',
+  serverUrl: serverUrl,
   options: {
     'sonar.projectDescription': 'This is a Node JS application',
     'sonar.projectName': 'Node JS Application - Sample',
     'sonar.projectKey': 'NodeJsMSS',
-    'sonar.login': 'sqa_5cbaeef3c359febf23be336c49424fffce37d09f',
+    'sonar.login': token,
     'sonar.projectVersion': '1.0',
     'sonar.language': 'js',
     'sonar.sourceEncoding': 'UTF-8',
-    'sonar.sources': 'src',  // Changed from '.' to 'src'
-    // 'sonar.tests': 'specs',
-    // 'sonar.inclusions': 'src/**'  // More precise inclusion pattern
+    'sonar.sources': '.',  // Corrected to root directory
+    'sonar.exclusions': 'node_modules/**'
   }
 }, () => {});
